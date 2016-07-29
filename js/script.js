1a85a9121b6103b9489792a4ea6838783ebb79d4
Anton <anton.verstka@gmail.com>
2016-07-29 18:54:00 +0300

    almost

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ $('.main-nav__toogle').click(function(){
 //    $('.main-nav__items-left').slideToggle();
 //    $('.main-nav__items-right').slideToggle();
 });
+
+$(document).ready(function(){
+  $('.single-slide').slick({
+      dots: true,
+//      accessibility: true,
+      arrows: true,
+//      centerMode: true, //уменьшает картинку, ставит в центре
+  });
+});
