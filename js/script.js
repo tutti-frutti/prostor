d34a61898d1fba9436100dd0e4c39df3e6465e5b
tutti-frutti <buzzloud.origin@gmail.com>
2016-07-31 10:48:29 +0300

    home jquery

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
-$('.main-nav__toogle').click(function(){
-   $('.main-nav').slideToggle(); 
+//$('.main-nav__toogle').click(function(){
+  // $('.main-nav').slideToggle(); 
 //    $('.main-nav__items-left').slideToggle();
 //    $('.main-nav__items-right').slideToggle();
-});
+//});
 
 $(document).ready(function(){
   $('.single-slide').slick({
       dots: true,
 //      accessibility: true,
       arrows: true,
+//      fade: true,
+//      autoplay: true,
+      prevArrow: '<img class="a" src="/img/arr-left.png">',
+      nextArrow: '<img class="b" src="/img/arr-right.png">',
 //      centerMode: true, //уменьшает картинку, ставит в центре
   });
+});
+//
+//$(document).ready(function() {
+//		$(".fancybox").fancybox();
+//	});
+
+$(document).ready(function() {   
+		$('.fancybox')
+            .attr('rel', 'gallery')
+            .fancybox({
+                padding : 3,
+                width   : '320',
+                closeBtn: false,
+        });
+	});
+
+$(function(){
+    $('#menu').slicknav({
+        duration: 300,
+        closedSymbol: "&#9660;",
+    });
 });
